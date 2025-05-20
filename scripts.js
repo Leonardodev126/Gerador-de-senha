46bcc1277ea72d5746ba4e20309400e8f5dddef2
Leonardo <leodeveloper12@gmail.com>
2025-05-20 10:33:42 -0300

    Adicionando JS ao projeto

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+
+const sliderElement = document.querySelector("#slider");
+const buttonElement = document.querySelector("#button");
+const sizePassword = document.querySelector("#valor");
+const password = document.querySelector("#password");
+const containerPasswod = document.querySelector("container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.ariaValueMax;
